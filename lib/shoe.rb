93e017b8e4191d54ef343a9c5b75dbbd4243ce02
Ming Duan <11203duan@gmail.com>
2019-03-03 01:54:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-    BRANDS = BRANDS.uniq
+    BRANDS.uniq
   end
 
   def cobble
@@ -17,7 +17,7 @@ class Shoe
   def brand=(input)
     @brand = input
     BRANDS << input
-    BRANDS = BRANDS.uniq
+    BRANDS.uniq
   end
 
   def brand
